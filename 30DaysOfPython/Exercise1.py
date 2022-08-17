f650400758cec196a766211338d3f407e76cd74f
arthur-chen-2018 <chenarthur@gmail.com>
2022-08-17 21:39:34 +1000

    day 2

---
+++ 30DaysOfPython/Exercise1.py
@@ -0,0 +1,18 @@
+from time import process_time_ns
+
+
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)
+print(3/4)
+print(3**4)
+print(3//4)
+
+print("'Arthur'")
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh','Python','Finland']))
